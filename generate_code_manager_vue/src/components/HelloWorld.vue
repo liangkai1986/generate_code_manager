b5247c5331343e1847492bc2add40bd66b5c2616
小旋风 <1945605733@qq.com>
2018-06-14 16:19:04 +0800

    初始提交项目

---
+++ generate_code_manager_vue/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <div class="hello">
+    <h1 v-on:click="a()">{{ msg }}aa</h1>
+        <img src="@/staticss/layui/images/face/66.gif">
+  </div>
+</template>
+
+
+
+
+
+  
+<script>
+import '@/staticss/layui/layui.all.js'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },methods:{
+    a(){
+      //alert("aaaaa");
+console.log("aaaaa");  
+
+  layer.msg('Hello World');
+      layer.msg("aaaaaaaa");
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
