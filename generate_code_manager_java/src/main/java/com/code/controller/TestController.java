ad0f5679907e004965df40044b51deec2a41e93d
小旋风 <1945605733@qq.com>
2018-06-14 15:46:48 +0800

    初始化

--- generate_code_manager_java/src/main/java/com/code/controller/TestController.java
+++ generate_code_manager_java/src/main/java/com/code/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.code.controller;
 
 public class TestController extends BaseController{
+	public void index() {
+		renderText("这是index");
+	}
 	public void test() {
 		renderText("这是test");
-//		DbKit.getConfig().getConnection();
 	}
 }
